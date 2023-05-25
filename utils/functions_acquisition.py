d2331b998681975c13f002252f3f664582279f7c
ayto <arouxel@laas.fr>
2023-05-25 19:33:59 +0300

    solve issue #3 : When scene is smaller the the detector FOV, add zeros in the scene

--- utils/functions_acquisition.py
+++ utils/functions_acquisition.py
@@ -24,10 +24,15 @@ def get_measurement_in_3D(scene, filtering_cube,chunk_size):
     return measurement_in_3D
 
 
-def crop_scene(scene, filtering_cube):
+def match_scene_to_instrument(scene, filtering_cube):
     if filtering_cube.shape[0] != scene.shape[0] or filtering_cube.shape[1] != scene.shape[1]:
+        if scene.shape[0] < filtering_cube.shape[0]:
+            scene = np.pad(scene, ((0, filtering_cube.shape[0] - scene.shape[0]), (0, 0), (0, 0)), mode="constant")
+        if scene.shape[1] < filtering_cube.shape[1]:
+            scene = np.pad(scene, ((0, 0), (0, filtering_cube.shape[1] - scene.shape[1]), (0, 0)), mode="constant")
         scene = scene[0:filtering_cube.shape[0], 0:filtering_cube.shape[1], :]
         print("Filtering cube and scene must have the same lines and columns")
+
 
         if filtering_cube.shape[2] != scene.shape[2]:
             scene = scene[:, :, 0:filtering_cube.shape[2]]
