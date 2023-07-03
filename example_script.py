cf8f5df43045c831bbd3ea12ba76ccd235e7eb74
ayto <arouxel@laas.fr>
2023-07-04 02:26:28 +0300

    Run cassi in script : first complete working script

--- example_script.py
+++ example_script.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import time
 from CassiSystem import CassiSystem
 from utils import *
 
@@ -9,33 +11,32 @@ scene_directory = "./datasets/"
 scene_name = "PaviaU"
 results_directory = "experiment_results"
 
+if __name__ == '__main__':
+
+    cassi_system = CassiSystem(system_config_path="config/cassi_system.yml")
 
 
-cassi_system = CassiSystem(system_config_path="config/cassi_system.yml")
+    cassi_system.load_scene(scene_name, scene_directory)
+
+    cassi_system.create_dmd_mask()
+    cassi_system.generate_2D_mask(simulation_config["mask"]["type"],simulation_config["mask"]["slit position"],simulation_config["mask"]["slit width"])
+
+    cassi_system.propagate_mask_grid([system_config["spectral range"]["wavelength min"],
+                                                            system_config["spectral range"]["wavelength max"]],
+                                                            system_config["spectral range"]["number of spectral samples"])
 
 
-cassi_system.load_scene(scene_name, scene_directory)
-
-cassi_system.create_dmd_mask()
-cassi_system.generate_2D_mask(simulation_config["mask"]["type"],simulation_config["mask"]["slit position"],simulation_config["mask"]["slit width"])
-
-cassi_system.propagate_mask_grid([system_config["spectral range"]["wavelength min"],
-                                                        system_config["spectral range"]["wavelength max"]],
-                                                        system_config["spectral range"]["number of spectral samples"])
-# cassi_system.generate_filtering_cube()
+    cassi_system.generate_filtering_cube()
 #
-# scene = match_scene_to_instrument(cassi_system, filtering_cube)
-#
-# self.finished_interpolated_scene.emit(scene)
-#
-# # Define chunk size
-# chunk_size = 50  # Adjust this value based on your system's memory
-#
-# t_0 = time.time()
-# measurement_in_3D = get_measurement_in_3D(scene, filtering_cube, chunk_size)
-# print("Acquisition time: ", time.time() - t_0)
-#
-# self.last_measurement_3D = measurement_in_3D
-# self.interpolated_scene = scene
+    interpolated_scene = match_scene_to_instrument(cassi_system.scene, cassi_system.filtering_cube)
+    chunk_size = 50  # Adjust this value based on your system's memory
+    measurement_in_3D = get_measurement_in_3D(interpolated_scene, cassi_system.filtering_cube, chunk_size)
+
+    last_measurement_3D = measurement_in_3D
+
+    plt.imshow(np.sum(measurement_in_3D[:,:],axis=2))
+    plt.show()
+
+
 #
 # self.finished_acquire_measure.emit(self.last_measurement_3D)  # Emit a tuple of arrays
