ba880d6c7ed40e0f2181610af402336562f9b31c
Antoine Rouxel <arouxel@laas.fr>
2023-07-21 13:57:50 +0200

    Adding multi-acquisitions script

---
+++ multi_acq_script.py
@@ -0,0 +1,47 @@
+from CassiSystem import CassiSystem
+from utils.helpers import load_yaml_config
+import os
+
+config_dataset = load_yaml_config("config/dataset.yml")
+config_system = load_yaml_config("config/cassi_system.yml")
+config_masks = load_yaml_config("config/filtering.yml")
+config_acquisition = load_yaml_config("config/acquisition.yml")
+
+dataset_name = "PaviaU"
+results_directory = "./data/results/testing_stuff_2"
+nb_of_acq = 10
+
+if __name__ == '__main__':
+
+    # Initialize the CASSI system
+    cassi_system = CassiSystem(system_config=config_system)
+
+    # DATASET : Load the hyperspectral dataset
+    cassi_system.load_dataset(dataset_name, config_dataset["datasets directory"])
+
+    for i in range (nb_of_acq):
+        # MASK : Generate the dmd mask
+        cassi_system.generate_2D_mask(config_masks)
+
+        # PROPAGATION : Propagate the mask grid to the detector plane
+        cassi_system.propagate_mask_grid()
+
+        # FILTERING CUBE : Generate the filtering cube
+        cassi_system.generate_filtering_cube()
+
+        # ACQUISITION : Simulate the acquisition with psf (use_psf is optional)
+        cassi_system.image_acquisition(use_psf=False,chunck_size=50)
+
+        # Save the acquisition
+        cassi_system.result_directory =results_directory
+        os.makedirs(results_directory, exist_ok=True)
+
+        if i == 0:
+            cassi_system.save_interpolated_scene("interpolated_scene")
+            cassi_system.save_panchromatic_image("panchro")
+            cassi_system.save_wavelengths("wavelengths")
+
+        cassi_system.save_measurement(f"measurement_{i}")
+        cassi_system.save_filtering_cube(f"filtering_cube_{i}")
+        cassi_system.save_mask(f"mask_{i}")
+
