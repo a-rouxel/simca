8e7852fd851b0bdda520f4f1fc3b6ad71d4a9e2e
Antoine Rouxel <arouxel@laas.fr>
2023-08-03 14:11:29 +0200

    solve minor issue #2

--- cassi_systems/CassiSystem.py
+++ cassi_systems/CassiSystem.py
@@ -723,8 +723,10 @@ class CassiSystem():
         save_config_mask_and_filtering("config_mask_and_filtering",config_mask_and_filtering,self.result_directory)
         save_config_acquisition("config_acquisition",config_acquisition,self.result_directory)
         save_interpolated_scene("interpolated_scene",self.interpolated_scene, self.result_directory)
-        if self.scene_labels is not None:
+        try:
             save_interpolated_scene_labels("scene_labels",self.scene_labels,self.result_directory)
+        except :
+            pass
         save_filtered_interpolated_scene("filtered_interpolated_scene",self.last_filtered_interpolated_scene, self.result_directory)
         save_measurement("measurement",self.measurement,self.result_directory)
         save_panchromatic_image("panchro",self.panchro,self.result_directory)
