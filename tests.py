d4f00fd620abb285bb46b766b2626df9363c9376
Antoine Rouxel <arouxel@laas.fr>
2023-10-05 10:51:20 +0200

    removing useless tests

--- tests.py
+++ tests.py
@@ -1,7 +1,5 @@
-from simca.tests import test_cassisystem_initialization, test_functions_acquisition
+from simca.tests import test_cassisystem_initialization
 import unittest
 
 if __name__ == '__main__':
     unittest.main(test_cassisystem_initialization, exit=False)
-
-    unittest.main(test_functions_acquisition)
