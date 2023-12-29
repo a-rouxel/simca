855c6c0ee647b1ed80e8cc1a71ea3388b11192d3
antoine rouxel <arouxel@laas.fr>
2023-12-29 19:30:37 +0100

    removing torch related content

--- gui_elements/__init__.py
+++
@@ -1,5 +0,0 @@
-from .EditorSystemConfigWidget import EditorSystemConfigWidget
-from .OpticsWidget import OpticsWidget
-from .FilteringCubeWidget import FilteringCubeWidget
-from .DatasetWidget import DatasetWidget
-from .AcquisitionWidget import AcquisitionWidget
