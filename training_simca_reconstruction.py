bed97ca44c70ac21a1b9e0f27855fa70caadf364
antoine rouxel <arouxel@laas.fr>
2024-03-06 22:14:17 +0100

    adding training script and Main lightning module with all operations

---
+++ training_simca_reconstruction.py
@@ -0,0 +1,18 @@
+import pytorch_lightning as pl
+from data_handler import CubesDataModule
+from optimization_modules import JointReconstructionModule_V1
+
+data_dir = "./datasets_reconstruction/"
+
+datamodule = CubesDataModule(data_dir, batch_size=2, num_workers=1)
+
+name = "testing_simca_reconstruction"
+
+
+reconstruction_module = JointReconstructionModule_V1()
+
+trainer = pl.Trainer( accelerator="cpu",
+                        max_epochs=500,
+                        log_every_n_steps=100)
+
+trainer.fit(reconstruction_module, datamodule)
